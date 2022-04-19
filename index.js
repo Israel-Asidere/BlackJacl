2c131d2d7d594ec0c2183a2e294850e748914740
Israel Asidere <israelasidere@yahoo.com>
2022-04-19 15:19:04 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let firstCard = 10
+let secondCard = 2
+
+let sum = firstCard + secondCard
+
+if(sum <= 20){
+    console.log('Do you want to draw a new card?')
+}
+
+else if(sum === 21){
+console.log("Wohoo! you've got BlackJack")
+}
+
+else{
+    console.log("You're out of the game")
+}
+
+
+// let age = 22
+
+// if(age<21){
+//     console.log('You cannot enter the club!')
+// }
+// else {
+//     console.log('Welcome!')
+// }
+
+// let age = 100
+
+// if(age<100){
+//     console.log('you are not eligible')
+// }
+// else if (age =100){
+//     console.log('Here is your bithday card from the king')
+// }
+// else{
+//     console.log('Not elegible, you have already gotten one+ ')
+// }
