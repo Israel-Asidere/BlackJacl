5d17d0a11797744a1b6eaecad61e3966bc1d343e
Israel Asidere <israelasidere@yahoo.com>
2022-04-26 16:58:42 +0100

    setting first and second card to getRandomCard function. For Loops and Arrays

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-let firstCard = 10
-let secondCard = 2
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
 let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById('sum-el')
+let cardsEl = document.getElementById('cards-el')
+console.log(messageEl)
+
 
 function startGame(){
-    if(sum <= 20){
-        message ='Do you want to draw a new card?'
+    renderGame()
+} 
+
+function getRandomCard(){
+    return 5
+}
+function renderGame() {
+
+    cardsEl.textContent = 'Cards: '
+    for(let i=0; i<cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
     }
     
-    else if(sum === 21){
-    message = "Wohoo! you've got BlackJack"
-    hasBlackJack = true
-    }
-    
-    else{
-        message = "You're out of the game"
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = 'Do you want to draw a new card?'
+    } else if (sum === 21) {
+        message = "Wohoo! you've got BlackJack"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game   "
         isAlive = false
-    }
-    
-    console.log(message)
+    } messageEl.textContent = message
 }
 
+function newCard() {
+    console.log('Drawing a new card from the deck!')
+
+    let card = getRandomCard();
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
+
+
+    
+}
 
 
 // let age = 22
