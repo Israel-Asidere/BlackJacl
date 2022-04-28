8178417828f70b359805466cba20ff623775ee47
Israel Asidere <israelasidere@yahoo.com>
2022-04-28 16:45:27 +0100

    added person object to store name and coins. completed exercise

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
 console.log(messageEl)
+console.log(cards)
 
+let player = {
+    name:'Israel',
+    chips: 145
+}
 
-function startGame(){
+console.log(typeof(cards))
+
+let playerEl = document.getElementById('player-el')
+playerEl.textContent = player.name + ": $" + player.chips
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
-} 
+}
 
-function getRandomCard(){
-    return 5
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 14)
+
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 function renderGame() {
 
     cardsEl.textContent = 'Cards: '
-    for(let i=0; i<cards.length; i++){
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
-    
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = 'Do you want to draw a new card?'
@@ -38,16 +58,14 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log('Drawing a new card from the deck!')
-
-    let card = getRandomCard();
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
-
-
     
+    if (isAlive == true && hasBlackJack==false) {
+        let card = getRandomCard();
+        sum += card
+        cards.push(card)
+        renderGame()
+
+    }
 }
 
 
---
+++ rough.js
@@ -0,0 +1,27 @@
+// let places ={
+//     isAvailable:true, 
+//     tags:[Bayelsa, Nigeria,Africa],
+//     price: 200,
+//     realtorName: '   Chalmers'
+// }
+
+// console.log(places.realtorName + '$: ' + places.price)
+
+
+ let age = 15
+
+ if(age<6){
+     console.log('free entry for you kiddo')
+ }
+ else if(age>6 && age<17){
+     console.log('child disount for you kiddo')
+ }
+ else if(age>17 && age<26){
+     console.log('student price for you sir/ma')
+}
+else if(age>26 && age<66){
+    console.log('full price for you sir/ma')
+}
+else if(age>66){
+    console.log('senior citizen discount')
+}
